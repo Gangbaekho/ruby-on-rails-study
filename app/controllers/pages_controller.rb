8eb19da40a07dbf71c6ccd18970cfa276b11c5a7
kimjinsoo <roorooro9933@gmail.com>
2021-10-28 22:43:30 +0900

    add about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
+# < 이게 extends를 뜻하는 거라고 생각하면 됨.
+# 뭐 문법 자체는 나쁘지 않는 것 같다. extends는 쓰기 귀찮긴 하네 
 class PagesController < ApplicationController
     def home
+    end
+    
+    def about
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
+  # get 은 여기서 HTTP GET Method를 뜻한다.
+  # get 다음에 오는게 url이고 이것을 어느 컨트롤러에 연결할것인가가 to 이고 # 다음에 오는 것이
+  # method명이다.
+  # 이건 String 기반이라서 실수할 수 있는 여지가 많이 있는 것 같다.
+  # Spring을 사용하면 Annotation 기반이라서 더 좋은것 같다는 생각이 든다.
+  get 'about', to: 'pages#about'
 end
